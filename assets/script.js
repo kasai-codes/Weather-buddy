054d26b593d5664f5281a646d9259d610632f982
kasai <kasaipreston@gmail.com>
2021-12-29 18:09:19 -0800

    apikey issue

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,17 @@
-var searchBtn = document.getElementById("search-button")
-var userInput = document.getElementById("city-input")
-var currentWeatherDiv = document.getElementById("current-weather")
-var apiKey = c3ad8822ccefd5e84846e388debccc00
+var searchBtn = document.getElementById("search-button");
+var userInput = document.getElementById("city-input");
+var currentWeatherDiv = document.getElementById("current-weather");
+var apiKey = e24a48d117f9490365fee8813aa9239b;
 
 // city data
 function getCity(searchedCity) {
 
-var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ searchedCity + "&units=imperial&appid=" + apiKey
+var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ searchedCity + "&units=imperial&appid=" + apiKey;
 fetch(cityUrl)
 .then(function (response) {
 
   return response.json();
-  
-})
-.then(function (data) {
-console.log(data.main.temp)
-
-
-
-var currentWeather =`
+  var currentWeather =`
 <div id="current" class="card">
 <p> Temperature: ${data.main.temp}</p>
 
@@ -26,12 +19,19 @@ var currentWeather =`
 `;
 
 currentWeatherDiv.innerHTML = currentWeather
+})
+.then(function (data) {
+console.log(data.main.temp)
+
+})
+}
+
+
 
 // lat = data.coord.lat;
 // lon = data.coord.lon;
 
-})
-}
+
 
 // City weather data
 
